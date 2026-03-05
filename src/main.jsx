6e70c2cfef3cf1016808596eb781d76153813e06
kaunghtetzaw <kaunghtetzaw.inbox@gmail.com>
2026-03-06 03:05:28 +0700

    Remove Vercel analytics imports from code

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/react"; // ← ADD THIS
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <Analytics />
-    <SpeedInsights />
   </StrictMode>,
 );
